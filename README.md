07fab1b09921ce998f68018c0b4e85e042f07d15
Hiago Rios <hiagoriosc@gmail.com>
2024-01-27 19:23:43 -0300

    creating basic UI layout

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # DevLog
 
 An app for developers to log and track their spent hours on activities.
+
+# Tasks:
+- [x] Create basic layout
+- [] Integrate activity input with API
+- [] List activities from database
+- [] Create calendar to filter the list
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
+  template: `
+    <aside id="sidebar">
+      <header>
+        <h1>DevLog</h1>
+      </header>
+      <div id="calendar">TODO: activity calendar</div>
+    </aside>
+    <main id="content">
+      <form id="activity-form" [formGroup]="activityForm">
+        <label for="description-input">What are you working on?</label>
+        <input
+          id="description-input"
+          type="text"
+          formControlName="description"
+          placeholder="Start typing..."
+        />
+        <button type="button">
+          Track
+        </button>
+        <span *ngIf="activityForm.controls['minutesSpent'].value > 0">{{ activityForm.controls['minutesSpent'].value }} min </span>
+      </form>
+      <section id="activity-list">
+        <h1>Activities</h1>
+        <ul>
+          <li>Actually work</li>
+          <li>Make coffee</li>
+          <li>Petting the cats</li>
+        </ul>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'devlog';
+  activityForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.activityForm = this.formBuilder.group({
+      id: [],
+      description: ['', [Validators.required]],
+      minutesSpent: [0]
+    })
+  }
 }
